97d7521f6fc760eb7c6a497cd8d32cc67e748a69
ApotheosisCrows <90779268+ApotheosisCrows@users.noreply.github.com>
2021-12-16 09:22:02 -0500

    yes

--- Assets/Scripts/EnemyFollower.cs
+++ Assets/Scripts/EnemyFollower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour
+public class EnemyFollower : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
